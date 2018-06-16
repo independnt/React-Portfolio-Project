c34528e788eea76c786b071dd6b703567f37e8ea
independnt <gonzalezdennis6@gmail.com>
2018-06-16 17:52:20 -0400

    added routes and actions in controller to correspond

--- final-project-api/app/controllers/api/users_controller.rb
+++ final-project-api/app/controllers/api/users_controller.rb
@@ -1,7 +1,31 @@
-class API::Users < ApplicationController
+class Api::UsersController < ApplicationController
+  before_action :set_user, only:[:show, :destroy, :update]
+
 
   def index
-    
+    render json: User.all
+  end
+
+  def create
+    user = User.create(user_params)
+    render json: user
+  end
+
+  def show
+    render json: @user
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:id])
   end
 
 
