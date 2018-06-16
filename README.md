78abcc7071e98c22a8a523823d607d45ab0205c4
independnt <gonzalezdennis6@gmail.com>
2018-06-16 17:20:41 -0400

    added bcrypt for password encryption, user model, and migrated user data to db

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# React-Portfolio-Project
+Users to log in - authentication
+Users to search locations - call Google API and pull locations
+Users to can add locations to favorites - Favorites page
+Users can remove favorites 
--- final-project-api/app/controllers/application_controller.rb
+++ final-project-api/app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+
+  # def authentication_required
+  #   if !logged_in?
+  #     redirect_to root_path, flash: {notice: "You need to be logged in to do that!"}
+  #   end
+  # end
+  #
+  # def logged_in?
+  #   !!current_user
+  # end
+  #
+  # def current_user
+  #   @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  # end
+
 end
