fdfeaaa530d33ef91b818f211ff36345552b5b89
independnt <gonzalezdennis6@gmail.com>
2018-06-19 17:35:46 -0400

    added redux devtools extension

--- final-project-client-side/src/store/store.js
+++ final-project-client-side/src/store/store.js
@@ -6,4 +6,4 @@ import { rootReducer } from '../reducers/index'
 
 const middleware = applyMiddleware(promise(), thunk)
 
-export default createStore(reducers, middleware)
+export default createStore(reducers, middleware, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
