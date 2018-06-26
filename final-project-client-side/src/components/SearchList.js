9e3c1b7f6d6d72b2c2b3a9422da470b5e4347a95
independnt <gonzalezdennis6@gmail.com>
2018-06-26 16:27:57 -0400

    changed searchlist to component, added button to add locations to favorites

--- final-project-client-side/src/components/SearchList.js
+++ final-project-client-side/src/components/SearchList.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-const SearchList = ({locations}) => {
-    const listResult = locations.map(location => (
-    <li key={location.id}>
-      Name: {location.name}<br/>
-      City/State: {location.city}/{location.state}<br/>
-      Street: {location.street}<br/>
-      Phone: {location.phone}<br/>
-      Website: <a href={`http://` + location.url} target="_blank">{location.url}</a><br/>
-    </li>
-  )
-)
 
+class SearchList extends React.Component{
+  handleAdd = (event) => {
+    event.preventDefault();
+  }
+  render(){
+
+    const listResults = this.props.locations.map(location => (
+           <li key={location.id}>
+             Name: {location.name}<br/>
+             City/State: {location.city}/{location.state}<br/>
+             Street: {location.street}<br/>
+             Phone: {location.phone}<br/>
+             Website: <a href={`http://` + location.url} target="_blank">{location.url}</a><br/>
+             <button onClick={this.handleAdd}>Add to Favorites</button>
+           </li>
+         )
+       )
 
     return (
       <div>
         <ul>
-          {listResult}
+          {listResults}
         </ul>
       </div>
 
-    );
-};
+    )
+  }
+}
 
 export default SearchList
