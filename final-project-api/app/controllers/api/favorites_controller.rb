7afcfcf3b09530cd17eddd537a7bda44c5f1bc6e
independnt <gonzalezdennis6@gmail.com>
2018-06-26 19:52:02 -0400

    added routes for favorites and logic for said routes

--- final-project-api/app/controllers/api/favorites_controller.rb
+++ final-project-api/app/controllers/api/favorites_controller.rb
@@ -1,3 +1,31 @@
 class Api::FavoritesController < ApplicationController
+  before_action :authenticate_user
 
-end 
+  def index
+    @user = User.find_by(id: params[:user_id])
+    @favorites = @user.favorites
+    render json: @favorites
+  end
+
+  def create
+    @user = User.find_by(id: params[:user_id])
+    @favorite = @user.favorites.build(favorite_params)
+    if @favorite.save
+      render json: @favorite
+    else
+      render json: {message: @favorite.errors}, status: 400
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:user_id])
+    @favorite = @user.favorites.find_by(id: params[:id])
+    @favorite.destroy
+  end
+
+  private
+
+  def favorite_params
+    params.require(:favorite).permit(:name, :city, :state, :street, :phone, :url)
+  end
+end
--- final-project-api/config/routes.rb
+++ final-project-api/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :users
     post 'user_token' => 'user_token#create'
+    resources :users do
+      resources :favorites, only: [:index, :create, :destroy]
+    end
   end
 end
