940a8360d59f42b1d71da879d9c523a7953fe2c1
independnt <gonzalezdennis6@gmail.com>
2018-06-28 20:17:42 -0400

    added validations for user and favorites.js container

--- final-project-api/app/models/user.rb
+++ final-project-api/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-  has_secure_password 
+  has_secure_password
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :username, uniqueness: true 
   has_many :favorites
 end
